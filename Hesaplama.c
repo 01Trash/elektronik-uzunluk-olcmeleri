470d8638ff56801a6faf54be1e30eb79b72fe3d2
Erdem <57351007+ErdemCmd@users.noreply.github.com>
2020-11-03 18:39:08 +0300

    Update Hesaplama.c

--- Hesaplama.c
+++ Hesaplama.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int x;
+    int x, y, z;
     long double c_Zero =  299792458; //c_Zero (boşlukta ışık hızı, m/s cinsinden)
     long double f, lamda_Zero; //f (frekans), lamda_Zero (lamda değeri)
     long double T; //T (periyod)
@@ -12,34 +12,51 @@ int main()
     printf("###ELEKTRONİK UZUNLUK ÖLÇMELERİ HESAPLAMALARI!!!###\n");
 
     // Neyi hesaplatmak istediğini sor.
-    printf("1 => Frekans hesaplaması,\n");
-    printf("2 => Periyod hesaplaması,\n");
-    printf("3 => Faz açısı hesaplaması,\n");
-    printf("Hesaplamak istediğin işlemi tuşla: ");
-    scanf("%d", &x);
+    printf("1 => ELEKTROMANYETİK DALGALARIN TEMELLERİ!!!\n");
+    printf("2 => KIRILMA İNDİSİ (n)!!!\n");
+    printf("Hangisi: ");
+    scanf("%d", &y);
+    if (y == 1) {
+        printf("1 => Frekans hesaplaması,\n");
+        printf("2 => Periyod hesaplaması,\n");
+        printf("3 => Faz açısı hesaplaması,\n");
+        printf("Hesaplamak istediğin işlemi tuşla: ");
+        scanf("%d", &x);
+    }
+    else if (y == 2) {
+        printf("1 => blaa blaa, \n");
+        scanf("%d", &z);
+    }
 
-    // Frekans hesaplanması, GHz cinsinden.
-    printf("lamda_Zero değerini Mikrometre(µm) cinsinden gir: ");
-    scanf("%Lf", &lamda_Zero);
-    f = c_Zero / (lamda_Zero * pow(10, -6));
-    if (x == 1) {
-        printf("f: %Lf GHz cinsinden.\n", f);
+    if (y == 1) {
+        // ELEKTROMANYETİK DALGALARIN TEMELLERİ!!!
+        // Frekans hesaplanması, GHz cinsinden.
+        printf("lamda_Zero değerini Mikrometre(µm) cinsinden gir: ");
+        scanf("%Lf", &lamda_Zero);
+        f = c_Zero / (lamda_Zero * pow(10, -6));
+        if (x == 1) {
+            printf("f: %Lf GHz cinsinden.\n", f);
+        }
+        // Periyod (T) hesaplaması, ns cinsinden.
+        T = 1 / (f * pow(10, -9));
+        if (x == 2) {
+            printf("T: %.25Lf ns cinsinden.\n", T);
+        }
+        // Faz açısı hesaplanması grad cinsinden.
+        printf("Işığın boşlukta aldığı yolu (s) metre cinsinden giriniz: ");
+        scanf("%Lf", &s);
+        faz_Grad = s / (lamda_Zero * pow(10, -6));
+        faz_Grad = faz_Grad * 400;
+        long double ceill(long double faz_Grad);
+        long double a = 400;
+        faz_Grad = fmod(faz_Grad, a);
+        if (x == 3) {
+            printf("faz_Grad: %.4Lf grad.\n", faz_Grad);
+        }
     }
-    // Periyod (T) hesaplaması, ns cinsinden.
-    T = 1 / (f * pow(10, -9));
-    if (x == 2) {
-        printf("T: %.25Lf ns cinsinden.\n", T);
-    }
-    // Faz açısı hesaplanması grad cinsinden.
-    printf("Işığın boşlukta aldığı yolu (s) metre cinsinden giriniz: ");
-    scanf("%Lf", &s);
-    faz_Grad = s / (lamda_Zero * pow(10, -6));
-    faz_Grad = faz_Grad * 400;
-    long double ceill(long double faz_Grad);
-    long double a = 400;
-    faz_Grad = fmod(faz_Grad, a);
-    if (x == 3) {
-        printf("faz_Grad: %.4Lf grad.\n", faz_Grad);
-    }
+
+    // KIRILMA İNDİSİ (n) (Bir dalganın ortamdaki yavaşlama katsayısı)
+    // Grup Kırılma İndisi (ng) hesaplanması
+    printf("lamda");
 
 }
